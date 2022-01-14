03fd8f0f2cfda285be2c8a98408b6f70cb8e6ead
ashutosh2468 <ashutoshkumarkarn01@gmail.com>
2022-01-14 18:58:12 +0530

    commpress fronted files

--- Server/index.js
+++ Server/index.js
@@ -3,75 +3,18 @@ const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
 const PORT = 4000;
-
 app.use(cors());
-
 app.listen(PORT, () => {
 	console.log(`Server Works !!! At port ${PORT}`);
 });
 app.use(express.json());
 app.use(express.static("public"));
-
 app.get("/",function(request,response){
 	response.sendFile(__dirname + "public/index.html");
 });
-
-
-
 app.get("/videoInfo",async function(request,response){
 	const videoURL = request.query.videoURL;
 	const info = await ytdl.getInfo(videoURL);
 	response.status(200).json(info);
 	console.log(info);
 });
-
-
-// app.get('/downloadmp3', async (req, res, next) => {
-// 	try {
-// 		var url = req.query.url;
-// 		if(!ytdl.validateURL(url)) {
-// 			return res.sendStatus(400);
-// 		}
-// 		let title = 'audio';
-
-// 		await ytdl.getBasicInfo(url, {
-// 			format: 'mp4'
-// 		}, (err, info) => {
-// 			if (err) throw err;
-// 			title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
-// 		});
-
-// 		res.header('Content-Disposition', `attachment; filename="${title}.mp3"`);
-// 		ytdl(url, {
-// 			format: 'mp3',
-// 			filter: 'audioonly',
-// 		}).pipe(res);
-
-// 	} catch (err) {
-// 		console.error(err);
-// 	}
-// });
-
-// app.get('/downloadmp4', async (req, res, next) => {
-// 	try {
-// 		let url = req.query.url;
-// 		if(!ytdl.validateURL(url)) {
-// 			return res.sendStatus(400);
-// 		}
-// 		let title = 'video';
-
-// 		await ytdl.getBasicInfo(url, {
-// 			format: 'mp4'
-// 		}, (err, info) => {
-// 			title = info.player_response.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
-// 		});
-
-// 		res.header('Content-Disposition', `attachment; filename="${title}.mp4"`);
-// 		ytdl(url, {
-// 			format: 'mp4',
-// 		}).pipe(res);
-
-// 	} catch (err) {
-// 		console.error(err);
-// 	}
-// });
